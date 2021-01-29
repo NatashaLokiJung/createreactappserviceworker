ff047c720008ffe10d6e99cda08e91cbdc798e35
NatashaLokiJung <427370@rts-365.dk>
2021-01-29 12:28:02 +0100

    ios support

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import OneSignal from 'react-onesignal';
 import Demo from "./components/Demo";
+import PWAPrompt from 'react-ios-pwa-prompt'
 
 OneSignal.initialize('dc279a24-1fae-4f6e-b013-0438fc509fec', {
   safari_web_id: "web.onesignal.auto.459ab5a0-25ed-43f1-a7b1-99d986ce9992"
@@ -26,6 +27,9 @@ function App() {
           Learn React
         </a>
       </header>
+      
+      <PWAPrompt promptOnVisit={1} timesToShow={3} copyClosePrompt="Close" permanentlyHideOnDismiss={false}/>
+
     </div>
   );
 }
